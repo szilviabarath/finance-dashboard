bf05c123eaa3da1618523843c6b3ce77315ce312
szilviabarath <szilvia.barat@gmail.com>
2023-02-15 17:36:38 +0100

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-   publicPath: process.env.NODE_ENV === "production" ? "szilviabarath.github.io/finance-dashboard" : "/",
+   publicPath:  'finance-dashboard',
 })
 
 
